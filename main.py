7d1363766e82584b3373a72a8b593fe7b2bac9d5
speria846 <niwenyesigasperia@gmail.com>
2022-05-08 18:05:56 +0300

    fourth commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import calendar
+print(calendar.weekheader(3))
+print()
+
+print(calendar.firstweekday())
+print()
+
+print(calendar.month (2020,3,))
+print(calendar.calendar(2022))
+print(calendar.calendar(2023))
